1a6ee492fd547cb0ea1425ef4f61cb41ec31364d
Ben Arc <ben@arc.wales>
2021-05-20 01:53:07 +0100

    Works better

--- bowser-web/src/layouts/MainLayout.vue
+++ bowser-web/src/layouts/MainLayout.vue
@@ -11,28 +11,7 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-show="currentRouteName"
-      v-model="leftDrawerOpen"
-      bordered
-      style="background-color: #1d1d1d"
-    >
-      <q-list>
-        <q-item-label header class="text-grey-8">
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-      <img
-        style="width: 50%"
-        class="fixed-bottom-left"
-        src="https://i.imgur.com/uJVGwhk.gif"
-      />
-    </q-drawer>
+
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -40,58 +19,11 @@
 </template>
 
 <script>
-import EssentialLink from "components/EssentialLink.vue";
-
-const linksData = [
-  {
-    title: "Docs",
-    caption: "quasar.dev",
-    icon: "school",
-    link: "https://quasar.dev"
-  },
-  {
-    title: "Github",
-    caption: "github.com/quasarframework",
-    icon: "code",
-    link: "https://github.com/quasarframework"
-  },
-  {
-    title: "Discord Chat Channel",
-    caption: "chat.quasar.dev",
-    icon: "chat",
-    link: "https://chat.quasar.dev"
-  },
-  {
-    title: "Forum",
-    caption: "forum.quasar.dev",
-    icon: "record_voice_over",
-    link: "https://forum.quasar.dev"
-  },
-  {
-    title: "Twitter",
-    caption: "@quasarframework",
-    icon: "rss_feed",
-    link: "https://twitter.quasar.dev"
-  },
-  {
-    title: "Facebook",
-    caption: "@QuasarFramework",
-    icon: "public",
-    link: "https://facebook.quasar.dev"
-  },
-  {
-    title: "Quasar Awesome",
-    caption: "Community Quasar projects",
-    icon: "favorite",
-    link: "https://awesome.quasar.dev"
-  }
-];
 
 export default {
   data() {
     return {
       leftDrawerOpen: false,
-      essentialLinks: linksData,
       showDraw: "fgndsf"
     };
   },
@@ -109,7 +41,6 @@ export default {
 
   },
   name: "MainLayout",
-  components: { EssentialLink },
 
   methods: {
     launchPinPad() {
